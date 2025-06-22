df2afcc532b231a661c904d389aa91d0fce7a193
Luc Mally <96879757+lucm9@users.noreply.github.com>
2025-06-22 18:15:46 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "mrcloudbook-cicd-bucket"
+    bucket         = "terraform-buck-luc"
     key            = "my-terraform-environment/main"
-    region         = "ap-south-1"
-    dynamodb_table = "mrcloudbook-dynamo-db-table"
+    region         = "us-east-2"
+    dynamodb_table = "terraform-locks"
   }
 }
