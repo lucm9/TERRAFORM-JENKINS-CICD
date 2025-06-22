144284f1488ba9cd4e380d0dfc69aaeb291c1449
Luc Mally <96879757+lucm9@users.noreply.github.com>
2025-06-22 18:16:35 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "aws_region" {
   description = "The AWS region to create things in."
-  default     = "ap-south-1"
+  default     = "us-east-2"
 }
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     = "Mumbai"
+  default     = "Leeno-pc.pem"     #change key name here
 }
 variable "instance_type" {
   description = "instance type for ec2"
@@ -14,10 +14,10 @@ variable "ami_id" {
   description = "AMI for Ubuntu Ec2 instance"
   default     = "ami-0f5ee92e2d63afc18"
 }
-variable "bucket_name" {
+variable "bucketname" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "mrcloudbook-777-ajaykumar-yegireddi"
+  default     = "jenkinscicd-533267047415-cloud"
 }
 
 variable "acl" {
