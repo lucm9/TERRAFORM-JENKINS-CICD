542590feb3473637a3d66bd2d037cf7be6854cb2
lucm9 <lucmally@yahoo.com>
2025-06-22 22:49:54 -0500

    mfa_delete = "Disabled"

--- s3.tf
+++ s3.tf
@@ -3,12 +3,44 @@ resource "aws_s3_bucket" "my_bucket" {
   acl    = var.acl
 }
 
-# sonar:ignore S6275
 resource "aws_s3_bucket_versioning" "this" {
   bucket = aws_s3_bucket.my_bucket.id
 
   versioning_configuration {
-    status = "Enabled"
-    # MFA-Delete suppressed - low-risk environment
+    status     = "Enabled"
+    mfa_delete = "Disabled"  # Explicitly documented
   }
+}
+
+# Compensating control: MFA protection via bucket policy
+data "aws_iam_policy_document" "deny_delete_without_mfa" {
+  statement {
+    sid    = "DenyDeleteWithoutMFA"
+    effect = "Deny"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:DeleteObject",
+      "s3:DeleteObjectVersion"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.my_bucket.arn}/*"
+    ]
+
+    condition {
+      test     = "Bool"
+      variable = "aws:MultiFactorAuthPresent"
+      values   = ["false"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "require_mfa_delete" {
+  bucket = aws_s3_bucket.my_bucket.id
+  policy = data.aws_iam_policy_document.deny_delete_without_mfa.json
 }
