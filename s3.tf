a7a0c4496f565ce65cd6d5af528b058cd9d606d9
lucm9 <lucmally@yahoo.com>
2025-06-22 22:28:25 -0500

    status = "Enabled"

--- s3.tf
+++ s3.tf
@@ -1,6 +1,10 @@
 resource "aws_s3_bucket" "my_bucket" {
   bucket = var.bucketname
   acl    = var.acl
+
+  tags = {
+    Name = "MyBucket"
+  }
 }
 
 resource "aws_s3_bucket_versioning" "this" {
@@ -8,5 +12,9 @@ resource "aws_s3_bucket_versioning" "this" {
 
   versioning_configuration {
     status = "Enabled"
+
+    # MFA delete is optional and only supported if versioning is enabled
+    # Note: Terraform cannot enable MFA Delete (AWS only supports enabling it via AWS CLI with root account)
+    # mfa_delete = "Enabled"  # This is not supported in Terraform
   }
 }
