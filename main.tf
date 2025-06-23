3320c21d905dbde8f1a1c12a629921242286d1c5
lucm9 <lucmally@yahoo.com>
2025-06-22 19:07:48 -0500

    remove ipv6

--- main.tf
+++ main.tf
@@ -35,7 +35,6 @@ resource "aws_security_group" "ec2_security_group" {
     to_port          = 443
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   ingress {
@@ -44,7 +43,7 @@ resource "aws_security_group" "ec2_security_group" {
     to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+  
   }
 
   egress {
@@ -52,7 +51,6 @@ resource "aws_security_group" "ec2_security_group" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
