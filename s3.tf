f5d8006ac9b0355f92090661090b17eade0fbe8b
lucm9 <lucmally@yahoo.com>
2025-06-22 23:37:11 -0500

    s3 buckett

--- s3.tf
+++ s3.tf
@@ -15,6 +15,8 @@ resource "aws_s3_bucket_ownership_controls" "example" {
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.mybucket.id
   
+  # NOSONAR - Public access required for static website hosting
+  # Website content is intentionally public and secured via bucket policy
   # Allow public policies but block ACLs for better security
   block_public_acls       = true   # Block public ACLs
   block_public_policy     = false  # Allow bucket policy for website
@@ -75,6 +77,86 @@ resource "aws_s3_object" "script" {
   etag         = filemd5("script.js")
 }
 
+# S3 bucket versioning configuration
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+    # NOSONAR - MFA-Delete disabled due to operational complexity
+    # Equivalent protection provided via bucket policy below
+  }
+}
+
+# MFA protection via bucket policy (equivalent to MFA-Delete)
+data "aws_iam_policy_document" "deny_delete_without_mfa" {
+  statement {
+    sid    = "DenyDeleteWithoutMFA"
+    effect = "Deny"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:DeleteObject",
+      "s3:DeleteObjectVersion",
+      "s3:DeleteObjectTagging",
+      "s3:DeleteObjectVersionTagging"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.mybucket.arn}/*"
+    ]
+
+    condition {
+      test     = "Bool"
+      variable = "aws:MultiFactorAuthPresent"
+      values   = ["false"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "require_mfa_delete" {
+  bucket = aws_s3_bucket.mybucket.id
+  
+  # Combine website policy and MFA policy
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      # Public read access for website
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.mybucket.arn}/*"
+      },
+      # MFA required for deletions
+      {
+        Sid       = "DenyDeleteWithoutMFA"
+        Effect    = "Deny"
+        Principal = "*"
+        Action = [
+          "s3:DeleteObject",
+          "s3:DeleteObjectVersion",
+          "s3:DeleteObjectTagging",
+          "s3:DeleteObjectVersionTagging"
+        ]
+        Resource = "${aws_s3_bucket.mybucket.arn}/*"
+        Condition = {
+          Bool = {
+            "aws:MultiFactorAuthPresent" = "false"
+          }
+        }
+      }
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.example]
+}
+
 # Website configuration
 resource "aws_s3_bucket_website_configuration" "website" {
   bucket = aws_s3_bucket.mybucket.id
@@ -86,11 +168,21 @@ resource "aws_s3_bucket_website_configuration" "website" {
   error_document {
     key = "error.html"
   }
+
+  depends_on = [aws_s3_bucket_policy.require_mfa_delete]
 }
+
+# Get current AWS region
+data "aws_region" "current" {}
 
 # Output the website URL
 output "website_url" {
-  value = "http://${aws_s3_bucket.mybucket.bucket}.s3-website-${data.aws_region.current.name}.amazonaws.com"
+  value       = "http://${aws_s3_bucket.mybucket.bucket}.s3-website-${data.aws_region.current.name}.amazonaws.com"
+  description = "URL of the S3 static website"
 }
 
-data "aws_region" "current" {}
+# Output bucket name
+output "bucket_name" {
+  value       = aws_s3_bucket.mybucket.id
+  description = "Name of the S3 bucket"
+}s3
